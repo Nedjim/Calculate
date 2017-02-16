320fbbd004c7d48f7a756afa565cc28e8c8a9e6f
Nedjim DANIMON NGABA <nedjim.danimon@gmail.com>
2017-02-16 03:47:56 +0100

    ADD: display number

--- src/app/Components/Calculator/Calculator.jsx
+++ src/app/Components/Calculator/Calculator.jsx
@@ -1,40 +1,54 @@
 import React from 'react';
 
 export default class Calculator extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            displayValue: '0'
+        }
+    }
+
+    inputDigit(e){
+        let digit = parseInt(e.target.value);
+        let currentValue = this.state.displayValue;
+        this.setState({
+            displayValue: parseInt(currentValue) === 0 ? digit : currentValue + e.target.value
+        });
+    }
+
     render(){
         return (
             <div id='calculate'>
+                <div className='line'>{this.state.displayValue}</div>
                 <div className='line'>
-                    0
+                    <button>AC</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                    <button>/</button>
                 </div>
                 <div className='line'>
-                    <div>AC</div>
-                    <div>+/-</div>
-                    <div>%</div>
-                    <div>/</div>
+                    <button onClick={this.inputDigit.bind(this)} value='7'>7</button>
+                    <button onClick={this.inputDigit.bind(this)} value='8'>8</button>
+                    <button onClick={this.inputDigit.bind(this)} value='9'>9</button>
+                    <button>X</button>
                 </div>
                 <div className='line'>
-                    <div>7</div>
-                    <div>8</div>
-                    <div>9</div>
-                    <div>X</div>
+                    <button onClick={this.inputDigit.bind(this)} value='4'>4</button>
+                    <button onClick={this.inputDigit.bind(this)} value='5'>5</button>
+                    <button onClick={this.inputDigit.bind(this)} value='6'>6</button>
+                    <button>-</button>
                 </div>
                 <div className='line'>
-                    <div>4</div>
-                    <div>5</div>
-                    <div>6</div>
-                    <div>-</div>
+                    <button onClick={this.inputDigit.bind(this)} value='1'>1</button>
+                    <button onClick={this.inputDigit.bind(this)} value='2'>2</button>
+                    <button onClick={this.inputDigit.bind(this)} value='3'>3</button>
+                    <button>+</button>
                 </div>
                 <div className='line'>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>+</div>
-                </div>
-                <div className='line'>
-                    <div>0</div>
-                    <div>-</div>
-                    <div>=</div>
+                    <button onClick={this.inputDigit.bind(this)} value='0'>0</button>
+                    <button>-</button>
+                    <button>=</button>
                 </div>
             </div>
         )
